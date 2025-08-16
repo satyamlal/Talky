44883079442d9b1391a9d936a86a2a2e406206b6
Satyam Lal <ssatyamlal@gmail.com>
2025-08-16 12:59:15 +0530

    Now user can join rooms

--- server/index.ts
+++ server/index.ts
@@ -2,18 +2,36 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket, req) => {
-  allSockets.push(socket);
-  userCount++;
-  console.log("Connected User #" + userCount);
-
   socket.on("message", (message) => {
-    for (let i = 0; i < allSockets.length; i++) {
-      const s = allSockets[i];
-      s?.send(message.toString() + " sent from the server!");
+    const parsedMessage = JSON.parse(message as unknown as string);
+
+    if (parsedMessage.type == "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+      });
     }
+
+    if (parsedMessage.type == "chat") {
+      let currentUserRoom = allSockets.find((x) => x.socket === socket)?.room;
+
+      allSockets.forEach((user) => {
+        if (user.room == currentUserRoom) {
+          user.socket.send(parsedMessage.payload.message);
+        }
+      });
+    }
+
+    socket.on("close", () => {
+      allSockets = allSockets.filter((x) => x.socket != socket);
+    });
   });
 });
