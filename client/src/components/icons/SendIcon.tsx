4eba2e48b3de6bbf9028db2fabd9573aa98ee50b
Satyam Lal <ssatyamlal@gmail.com>
2025-11-05 12:30:03 +0530

    update sendIcon orientation

--- client/src/components/icons/SendIcon.tsx
+++ client/src/components/icons/SendIcon.tsx
@@ -7,7 +7,12 @@ export function SendIcon(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        transform="rotate(90 12 12)"
+        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round" />
     </svg>
   );
 }
